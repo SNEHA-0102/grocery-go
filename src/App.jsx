2f6c1544dfd86155d4d321f587cf0bf304494f5a
SNEHA-0102 <snehaparab0102@gmail.com>
2025-04-25 19:21:36 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './index.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-green-100 to-blue-200">
+      <h1 className="text-4xl font-bold text-blue-800">
+        Hello World 👋
+      </h1>
+    </div>
+  )
+}
+
+export default App
